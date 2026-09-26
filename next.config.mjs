e7fb510ad4675801586d7d0ab015abc075978c39
Sunandar Ero <sunandar.ero@gmail.com>
2026-09-26 16:48:31 +0800

    fix: Re-apply Tailwind CSS fix and ESLint ignore

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
   // Abaikan error TS saat build di Railway agar build jauh lebih cepat & hemat RAM
   typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
   
   // Mengurangi penggunaan memori saat kompilasi
   experimental: {
